fc492f089b76a8a91894762b1fdb50c5f8793d37
cameili <camelliaz123@gmail.com>
2025-11-25 08:54:13 +0000

    .

--- resources/views/jurusan/index.blade.php
+++ resources/views/jurusan/index.blade.php
@@ -11,8 +11,6 @@
 
     <div class = "flex justify-between items-center mb-4">
         <a href="{{ route('jurusan.create') }}" class="px-4 py-2 rounded-lg text-[#fffdf2] bg-black  hover:scale-105 transition-all duration-200">Tambah Data</a>
- HEAD
-
         <div class = "mr-20">
         @if(session('success'))
             <p id="alert-message" class = "text-blue-500 text-xl">{{ session('success') }}</p>
